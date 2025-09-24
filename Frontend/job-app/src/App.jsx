b1b3bcb66bc2c2f5702928bb7feb47066168079f
Ashukla011 <shuklavashu9696@gmail.com>
2025-09-25 00:37:33 +0530

    ai-powered-job-recommendation-sy

---
+++ Frontend/job-app/src/App.jsx
@@ -0,0 +1,12 @@
+
+function App() {
+  return (
+    <div className="App">
+      <h1 class="text-3xl font-bold underline text-red-600">
+    Hello world!
+  </h1>
+    </div>
+  );
+}
+
+export default App;
